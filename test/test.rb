b2d9ece2c7d731a636ed782a41344e8f02294512
oddworldng <el.lobo.4ever@gmail.com>
2012-10-11 16:38:16 +0100

    gitignore subido

--- test/test.rb
+++ test/test.rb
@@ -10,6 +10,12 @@ r = c.radio(12)
 c.mostrar(r)
 
 
-# Prueba unitaria 1: recibe como parámetro un char
+# Prueba unitaria 2: recibe como parámetro un char
+a = "a"
+r = c.radio(a)
+c.mostrar(r)
+
+# Prueba unitaria 3: recibe como parámetro un string
+a = "hola"
 r = c.radio(a)
 c.mostrar(r)
