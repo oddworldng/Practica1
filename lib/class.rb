2f04e49606b97e5a43557dd4e7337c1709f6b76c
oddworldng <el.lobo.4ever@gmail.com>
2012-10-12 18:37:24 +0100

    subiendo...

--- lib/class.rb
+++ lib/class.rb
@@ -1,12 +1,5 @@
 #!/usr/bin/ruby
 
-
-#1) Escriba una clase con un método público que reciba como argumento el perímetro de una circunferencia y retorne el radio de la misma. Escriba pruebas unitarias para dicha clase. Compruebe el comportamiento ante situaciones como esta:
-#¿Qué ocurre si la entrada no es un número?
-#¿Qué ocurre si la entrada es un número negativo?
-#Escriba un fichero Rakefile de manera que al escribir en la línea de comandos rake test se ejecuten todas las pruebas que ha programado.
-#2) Indique la URL del repositorio github o bitbucket que contiene la solución que ha desarrollado.
-
 # define la clase Circunferencia  
 class Circunferencia  
  
